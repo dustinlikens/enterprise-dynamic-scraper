5285d3f4ae2965c4aca1c96c6fe353089c449204
dustinlikens <dustinlikens@icloud.com>
2017-10-18 23:15:02 -0700

    sleep 0.2 waiting for next month

--- app/application.rb
+++ app/application.rb
@@ -99,8 +99,8 @@ class Application
     #   sleep(0.05)
     # end
     # Capybara.using_wait_time(30) { page.body.include?('Next Month') }
-    puts "next month available"
-    # sleep(0.2)
+    # puts "next month available"
+    sleep(0.2)
     pickupDate = req.params['pickupDate']
     while !page.body.include?(pickupDate) do
     page.find('button[aria-label="Next Month"]').click
